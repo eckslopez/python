cc07ad17cf7ccdabf6d3fe306e328d77e325b8d5
Xavier Lopez <ecks910@yahoo.com>
2023-10-12 14:49:50 -0700

    a little more practice

---
+++ collection.py
@@ -0,0 +1,11 @@
+import urllib.request
+story = urllib.request.urlopen('http://sixty-north.com/c/t.txt')
+story_words = []
+
+for line in story:
+    line_words = line.decode('utf8').split()
+    for word in line_words:
+        story_words.append(word)
+
+story.close()
+print(story_words)
---
+++ map.py
@@ -0,0 +1,15 @@
+sizes = ['small','medium','large']
+colors = ['lavender','teal','burnt orange']
+animals = ['koala','platypus','salamander']
+
+#def combine(size,color,animal):
+#    return '{} {} {}'.format(size,color,animal)
+
+#print(list(map(combine,sizes,colors,animals)))
+
+#import itertools
+# Map will stop as soon as it encounters an infinite sequence
+#def combine(quantity,size,color,animal):
+#    return '{} {} {} {}'.format(quantity,size,color,animal)
+
+#print(list(map(combine,itertools.count(), sizes,colors,animals)))
